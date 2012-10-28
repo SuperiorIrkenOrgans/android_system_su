0348d912cf62095347e36827ea35799d801595d5
Earthnfire78 <earthnfire78@gmail.com>
2012-10-28 10:46:33 -0600

    Since the Gingerbread build of su does not fully work, lets us ChainsDD's compiled binary.

--- Android.mk
+++ Android.mk
@@ -1,15 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := su
-LOCAL_SRC_FILES := su.c db.c activity.c utils.c
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/su:system/xbin/su
 
 LOCAL_STATIC_LIBRARIES := \
     liblog \
     libc \
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := eng debug
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-
-include $(BUILD_EXECUTABLE)
